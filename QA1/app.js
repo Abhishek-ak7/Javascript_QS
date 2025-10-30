672240e95b969776e48a7697ffce58d4afcbc90a
Abhishekak71 <abhishekkumarms1621@gmail.com>
2025-10-30 13:36:43 +0530

    day1

---
+++ QA1/app.js
@@ -0,0 +1,60 @@
+/*Qs1. Write Qs1. Write a JavaScript program to get the first n elements of an array. [n can be any
+positive number].
+For example: for array [7, 9, 0, -2] and n=3
+Print, [7, 9, 0]a JavaScript program to get the first n elements of an array. [n can be any
+positive number].
+For example: for array [7, 9, 0, -2] and n=3
+Print, [7, 9, 0]
+*/
+
+// let arr=[7,9,0,-2];
+// let ans=arr.slice(0,3);
+// console.log(ans);
+
+// -------------------------------------------------------******------------------------------------------------------------------------------
+
+/*Qs2. Write a JavaScript program to get the last n elements of an array. [n can be any
+positive number].
+For example: for array [7, 9, 0, -2] and n=3
+Print, [9, 0, -2]*/
+
+// let arr1=[7,9,0,-2];
+// let n=prompt("Enter n value: ");
+// let r=arr1.length-n;
+// let ans1=arr1.slice(r);
+// console.log(ans1);
+
+// -------------------------------------------------------******------------------------------------------------------------------------------
+//  Write a JavaScript program to check whether a string is blank or not
+
+// let s=prompt("Enter a string: ");
+// if(s.length==0) console.log("String is empty");
+// else console.log("not emepty");
+
+// -------------------------------------------------------******------------------------------------------------------------------------------
+// Qs4. Write a JavaScript program to test whether the character at the given (character)
+// index is lower case.
+
+// let s=prompt("Enter a string: ");
+// let s1=s.toLocaleLowerCase();
+// let idx=prompt("Enter index: ");
+// if(s[idx]===s1[idx]) console.log("Lower case");
+// else console.log("Not in lower case");
+
+// -------------------------------------------------------******------------------------------------------------------------------------------
+
+// Qs6. Write a JavaScript program to check if an element exists in an array or not
+// function checkElement(arr, element) {
+//   // Using includes() method to check existence
+//   if (arr.includes(element)) {
+//     console.log(`${element} exists in the array.`);
+//   } else {
+//     console.log(`${element} does NOT exist in the array.`);
+//   }
+// }
+
+// // Example usage:
+// const numbers = [10, 20, 30, 40, 50];
+// const elementToCheck = 30;
+
+// checkElement(numbers, elementToCheck);
